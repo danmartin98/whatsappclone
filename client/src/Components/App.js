94d285db5726a604a77f6e326c948d6ef9d8a3ae
danmartin98 <danmartin98@icloud.com>
2022-03-29 14:33:52 +0100

    created id and conversations/contacts tab

---
+++ client/src/Components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+import Login from "./Login";
+import Dashboard from "./Dashboard"
+
+export default function App() {
+  const [id, setId] = useLocalStorage();
+  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+}
