07ba771794f00aa53b3f86148db529e489251889
danmartin98 <danmartin98@icloud.com>
2022-03-30 10:01:57 +0100

    wokring on newCoversationModal

--- client/src/Components/newConversationModal.js
+++ client/src/Components/newConversationModal.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+import contacts from "./Contacts";
+import { useContacts } from "../contexts/ContactsProvider";
 
 export default function NewConversationModal() {
-  return <div>newConversationModal</div>;
+  const [selectedContactIds, setSelectedContactIds] = useState([]);
+
+  function handleCheckboxChange(contactId) {
+    setSelectedContactIds((prevSelectedContactIds) => {
+      if (prevSelectedContactIds.includes(contactId)) {
+        return prevSelectedContactIds.filter((prevId) => {
+          return contactId !== prevId;
+        });
+      }
+    });
+  }
+  return (
+    <>
+      <Modal.Header closeButton>Create Conversation</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          {contacts.map((contact) => (
+            <Form.Group controlId={contact.id} key={contact.id}>
+              <Form.Check
+                type="checkbox"
+                value={selectedContactIds.includes(contact.id)}
+                label={contact.name}
+                onChange={() => handleCheckboxChange(contact.id)}
+              />
+            </Form.Group>
+          ))}
+          <Button type="submit" className="mt-3">
+            Create
+          </Button>
+        </Form>
+      </Modal.Body>
+    </>
+  );
 }
